21080766ace009182b8ebcd951d1cd08acaae76d
jongho <jj0101065@gmail.com>
2022-09-28 22:56:04 +0900

    세팅

---
+++ src/main/java/com/shop/myshop/MyshopBackApplication.java
@@ -0,0 +1,13 @@
+package com.shop.myshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshopBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyshopBackApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/shop/myshop/controller/Hello.java
@@ -0,0 +1,4 @@
+package com.shop.myshop.controller;
+
+public class Hello {
+}
