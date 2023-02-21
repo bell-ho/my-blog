f6f4eda016066cdc73371ff828af11180097d19e
bell-ho <jj0101065@gmail.com>
2023-02-21 13:38:31 +0900

    next-auth api 라우터

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,70 +4,79 @@ import GoogleProvider from 'next-auth/providers/google';
 import NaverProvider from 'next-auth/providers/naver';
 import { axios } from '@/util/axios';
 
-export default NextAuth({
-  providers: [
-    KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    NaverProvider({
-      clientId: process.env.NAVER_CLIENT_ID,
-      clientSecret: process.env.NAVER_CLIENT_SECRET,
-    }),
-  ],
-  session: {
-    jwt: true,
-  },
-  callbacks: {
-    async signIn(user, account, metadata) {
-      const provider = user.account.provider;
-
-      const nickName = await axios
-        .get(`https://nickname.hwanmoo.kr`, {
-          params: {
-            format: 'json',
-            max_length: 7,
-          },
-        })
-        .then((data) => data.data.words[0]);
-
-      const params = {
-        name: user.user?.name ?? '',
-        nickName,
-        uniqueKey: user.user?.id,
-        email: user.user?.email ?? '',
-        provider,
-      };
-
-      const validationUser = await axios
-        .get(`/api/v1/auth/validation-user/${params.uniqueKey}`)
-        .then((data) => data.data.data);
-
-      if (!validationUser?.member) {
-        const newMember = await axios
-          .post(`/api/v1/auth/signup`, params)
-          .then((data) => data.data.data);
-      }
-
-      // axios.defaults.headers.common['Authorization'] = `Bearer ${validationUser?.member.token}`;
-
-      return true;
+const nextAuthOptions = (req, res) => {
+  return {
+    providers: [
+      KakaoProvider({
+        clientId: process.env.KAKAO_CLIENT_ID,
+        clientSecret: process.env.KAKAO_CLIENT_SECRET,
+      }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+      NaverProvider({
+        clientId: process.env.NAVER_CLIENT_ID,
+        clientSecret: process.env.NAVER_CLIENT_SECRET,
+      }),
+    ],
+    session: {
+      jwt: true,
     },
-    async jwt({ token, account, user }) {
-      if (account) {
-        token.accessToken = account.access_token;
-        token.id = user?.id;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      session.user.id = token.id;
+    callbacks: {
+      async signIn(user, account, metadata) {
+        const provider = user.account.provider;
 
-      return session;
+        const nickName = await axios
+          .get(`https://nickname.hwanmoo.kr`, {
+            params: {
+              format: 'json',
+              max_length: 7,
+            },
+          })
+          .then((data) => data.data.words[0]);
+
+        const params = {
+          name: user.user?.name ?? '',
+          nickName,
+          uniqueKey: user.user?.id,
+          email: user.user?.email ?? '',
+          provider,
+        };
+
+        try {
+          const existUser = await axios
+            .get(`/api/v1/auth/validation-user/${params.uniqueKey}`)
+            .then((response) => {
+              res.setHeader('Set-Cookie', response.headers['set-cookie']);
+              return response.data.data;
+            });
+
+          if (!existUser) {
+            await axios.post(`/api/v1/auth/signup`, params).then((response) => {
+              res.setHeader('Set-Cookie', response.headers['set-cookie']);
+            });
+          }
+        } catch (e) {
+          console.error(e);
+        }
+        return true;
+      },
+      async jwt({ token, account, user }) {
+        if (account) {
+          token.accessToken = account.access_token;
+          token.id = user?.id;
+        }
+        return token;
+      },
+      async session({ session, token, user }) {
+        session.user.id = token.id;
+
+        return session;
+      },
     },
-  },
-});
+  };
+};
+export default (req, res) => {
+  return NextAuth(req, res, nextAuthOptions(req, res));
+};
--- src/util/axios.js
+++ src/util/axios.js
@@ -1,6 +1,9 @@
 import Axios from 'axios';
 import { backUrl } from '@/configs/backUrl';
+import { getSession } from 'next-auth/react';
+import { Cookies } from 'react-cookie';
 
+const cookies = new Cookies();
 export const axios = Axios.create({
   baseURL: backUrl,
   headers: {
@@ -11,8 +14,15 @@ export const axios = Axios.create({
 axios.defaults.withCredentials = true;
 
 axios.interceptors.request.use(
-  function (config) {
-    return config;
+  async function (request) {
+    const session = await getSession();
+
+    if (session) {
+      const token = cookies.get('Authorization');
+      request.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return request;
   },
   function (error) {
     return Promise.reject(error);
