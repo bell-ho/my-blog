99695ebb884a018e00886643a43ca8d4a150752c
bell-ho <jj0101065@gmail.com>
2023-02-27 17:01:26 +0900

    스크롤 탑 버트ㅗㄴ

---
+++ src/components/ui/ScrollToTopButton.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { css } from '@emotion/react';
+
+const ScrollToTopButton = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 100) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <button css={buttonStyle} style={{ display: visible ? 'block' : 'none' }} onClick={handleClick}>
+      Top
+    </button>
+  );
+};
+const buttonStyle = css`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 50px;
+  height: 50px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #555;
+  }
+`;
+export default ScrollToTopButton;
