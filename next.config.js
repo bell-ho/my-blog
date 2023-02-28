3570dd2e35eb3aff44d5b88e6224c76957501c6e
jongho <jj0101065@gmail.com>
2023-02-28 20:27:09 +0900

    public

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [process.env.IMAGES_DOMAIN],
+    domains: [process.env.NEXT_PUBLIC_IMAGES_DOMAIN],
   },
   compiler: {
     emotion: true,
